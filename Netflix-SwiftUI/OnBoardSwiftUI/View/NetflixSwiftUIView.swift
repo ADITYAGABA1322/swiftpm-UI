6018a9d1a15d51947f3bf017d35acd68c84c8b5d
“ADITYAGABA1322” <adityagaba1322@gmail.com>
2023-05-07 02:08:10 +0530

    added

---
+++ Netflix-SwiftUI/OnBoardSwiftUI/View/NetflixSwiftUIView.swift
@@ -0,0 +1,19 @@
+//
+//  NetflixSwiftUIView.swift
+//  OnBoardSwiftUI
+//
+//  Created by Aditya Gaba on 01/05/23.
+//
+
+import SwiftUI
+
+
+@main
+struct OnBoardSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
