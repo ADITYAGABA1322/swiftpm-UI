daed390f8e1019477146f538d38a5541189ae001
“ADITYAGABA1322” <adityagaba1322@gmail.com>
2023-06-19 18:50:41 +0530

    add

---
+++ Calculator/Model/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Aditya Gaba on 17/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
