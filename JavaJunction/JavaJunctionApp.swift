4b5bed0e57d20e6b4335b6fa6ad2ceda20b29657
Aditya <adityagaba1322@gmail.com>
2023-07-03 23:36:22 +0530

    add

---
+++ JavaJunction/JavaJunctionApp.swift
@@ -0,0 +1,17 @@
+//
+//  JavaJunctionApp.swift
+//  JavaJunction
+//
+//  Created by Aditya Gaba on 02/07/23.
+//
+
+import SwiftUI
+
+@main
+struct JavaJunctionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
